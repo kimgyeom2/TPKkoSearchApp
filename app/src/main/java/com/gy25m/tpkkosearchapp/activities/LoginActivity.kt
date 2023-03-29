ec4d1019a91c53e3e0d640edeed2dc3a2601a94f
kimgyeom2 <kimgyeom2@gmail.com>
2023-03-29 09:55:00 +0900

    SplashActivity 기능 구현

---
+++ app/src/main/java/com/gy25m/tpkkosearchapp/activities/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.gy25m.tpkkosearchapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gy25m.tpkkosearchapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+    }
+}
--- app/src/main/java/com/gy25m/tpkkosearchapp/activities/SplashActivity.kt
+++ app/src/main/java/com/gy25m/tpkkosearchapp/activities/SplashActivity.kt
@@ -1,12 +1,26 @@
 package com.gy25m.tpkkosearchapp.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.gy25m.tpkkosearchapp.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        //setContentView(R.layout.activity_splash) //테마를 이용하여 화면구성
+
+        // 1.5초 후에 로그인 화면으로 이동
+//        Handler(Looper.getMainLooper()).postDelayed(object : Runnable{
+//            override fun run() {
+//                TODO("Not yet implemented")
+//            }
+//        },1500)    람다로 축약
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,LoginActivity::class.java))
+            finish()
+        },1500)
     }
 }
